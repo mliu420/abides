d84f7bb4eae785283ffc99a8d2d6df894d783192
Mingchun Liu <mliu420@gatech.edu>
2019-11-11 15:47:26 -0700

    changes

--- contributed_traders/mliu420_blazeit/mliu420_blazeit.py
+++ contributed_traders/mliu420_blazeit/mliu420_blazeit.py
@@ -88,9 +88,9 @@ class mliu420_blazeit(TradingAgent):
                         bidVol = round(max(0, self.holdings['CASH']) / bidP / 2)
                         askVol = round(max(0, self.holdings['CASH']) / askP / 2)
                         try:
-                            print('bidvol, askvol, jpm',bidVol, askVol, self.holdings[self.symbol])
-                            bidVol += max(0, -self.holdings[self.symbol])
-                            askVol -= max(0, -self.holdings[self.symbol])
+                            print('bidvol, askvol, jpm, cash',bidVol, askVol, self.holdings[self.symbol],self.holdings['CASH'])
+                            bidVol = max(0, -self.holdings[self.symbol])
+                            askVol = max(0, self.holdings[self.symbol])
                             print('bidvol, askvol, jpm',bidVol, askVol, self.holdings[self.symbol])
                         except:
                             pass
