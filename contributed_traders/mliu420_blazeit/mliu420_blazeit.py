87a7de48e8c9e26932da58357dc0d635d5b7976b
Mingchun Liu <mliu420@gatech.edu>
2019-11-11 21:06:26 -0700

    s

--- contributed_traders/mliu420_blazeit/mliu420_blazeit.py
+++ contributed_traders/mliu420_blazeit/mliu420_blazeit.py
@@ -123,8 +123,8 @@ class mliu420_blazeit(TradingAgent):
                             pass
                         midM = (askM + bidM) / 2
                         midP = midM + self.stdS / 5 * bidVol / (bidVol + askVol) - self.stdS / 10
-                        bidP = math.floor(( (midP - self.stdS/1.5) * 5 + bidM) / 6)
-                        askP = math.ceil(( (midP + self.stdS/1.5) * 5 + askM) / 6)
+                        bidP = math.floor(( (midP - self.stdS/2) * 3 + bidM) / 4)
+                        askP = math.ceil(( (midP + self.stdS/2) * 3 + askM) / 4)
                         print('Algo Spread:',askP,bidP, askP - bidP)
                         if bidVol > 0:
                             self.placeLimitOrder(self.symbol, bidVol, True, bidP)
