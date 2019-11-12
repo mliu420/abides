2ead3e3d610ef8ee168ed4796dd48113389f56da
Mingchun Liu <mliu420@gatech.edu>
2019-11-11 18:48:41 -0700

    change

--- contributed_traders/mliu420_blazeit/mliu420_blazeit.py
+++ contributed_traders/mliu420_blazeit/mliu420_blazeit.py
@@ -102,9 +102,9 @@ class mliu420_blazeit(TradingAgent):
                         bidM = bidP
                         midP = (askM + bidM) / 2
                         askP = min(askM - 1, askP)
-                        askP = max(midP + 10, askP)
+                        askP = max(midP + 20, askP)
                         bidP = max(bidM + 1, bidP)
-                        bidP = min(midP - 10, bidP)
+                        bidP = min(midP - 20, bidP)
                         dist = askVol / (askVol + bidVol)
 #                         if dist > 0.65:
 #                             askP = round(0.9 * askP + (0.1) * midP)
